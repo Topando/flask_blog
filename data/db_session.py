066e22cce9029c2fbeb76cc3df704393d932e1f0
Topando <erdekov.sascha@yandex.ru>
2022-03-11 02:03:40 +0300

    make it better

--- data/db_session.py
+++ data/db_session.py
@@ -12,13 +12,15 @@ def global_init(db_file):
 
     if __factory:
         return
-    #postgresql+psycopg2://user:password@hostname/database_name
+    # postgresql+psycopg2://user:password@hostname/database_name
     if not db_file or not db_file.strip():
         raise Exception("Необходимо указать файл базы данных.")
     conn_str = f'sqlite:///{db_file.strip()}?check_same_thread=False'
     print(f"Подключение к базе данных по адресу {conn_str}")
 
-    engine = sa.create_engine("postgresql+psycopg2://postgres:28012005@localhost/db", echo=False)
+    engine = sa.create_engine(
+        "postgresql+psycopg2://tkrpujjplpturj:958a136f1723758f54029507c0575079f6972238217e5605ad1ac6a23a2231df@ec2-54-236-137-173.compute-1.amazonaws.com/dd48pmpea10n0l",
+        echo=False)
     __factory = orm.sessionmaker(bind=engine)
     from . import __all_models
 
